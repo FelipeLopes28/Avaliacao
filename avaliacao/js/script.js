c8c058960f0fd077c57d1372d672856d3e9cf5be
FelipeLopes28 <felipelopes182640@sou.urcamp.edu.br>
2020-05-29 06:47:49 -0300

    add script

---
+++ avaliacao/js/script.js
@@ -0,0 +1,58 @@
+        var trocaFundo = document.querySelector('.trocaFundo')
+        var trocaLogoSI = document.querySelector('.trocaLogoSI')
+        var trocaRodape = document.querySelector('.trocaRodape')
+        var apagacancelar = document.querySelector('.apagacancelar')
+        var enviar = document.querySelector('.enviar')
+        var cancelar = document.querySelector('.cancelar')
+
+        var matricula = document.querySelector('.matricula');
+        var senha = document.querySelector('.senha');
+
+
+
+trocaFundo.addEventListener('click', trocaFundoTodo);
+trocaRodape.addEventListener('click', trocaFundoRodape);
+trocaLogoSI.addEventListener('click', trocaLogotipo);
+apagacancelar.addEventListener('click', apagaCancelar);
+
+cancelar.addEventListener('click', limpar);
+enviar.addEventListener('click', verificaNumero);  
+        
+        
+        function verificaNumero(){
+        var regra = /^[0-9] +$/;
+        if (!matricula.value.match(regra)){
+                alert('Somente números!!');
+                limpar();
+            }
+        }
+
+        function limpar(){
+            senha.value = '';
+            matricula.value = '';
+        }
+
+        function apagaCancelar(){
+            limpar.hidden = true;
+        }
+
+
+        function trocaFundoTodo(){
+            document.body.style.backgroundColor = "#001111";
+        }
+
+        function trocaFundoRodape(){
+            document.querySelector('.rodape').style.backgroundColor = "#001111";
+        }
+
+        function trocaLogotipo(){
+            document.querySelector('.logo').src="./imagens/enviar.png";
+        }
+
+
+
+
+
+        
+
+        
